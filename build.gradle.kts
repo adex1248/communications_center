dc8f15cad7c497852e53ac210f3620295f3beaa5
adex1248 <blackholian1456@gmail.com>
2025-02-26 09:24:22 +0900

    hmm

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    //maven { url = URL("https://mvnrepository.com/artifact/org.apache.commons/commons-lang3") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("commons-cli:commons-cli:1.8.0")
+    //implementation("org.apache.commons:commons-math3:3.6.1")
+    //implementation("org.apache.commons:commons-lang3:3.15.0")
+    //implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("com.illposed.osc:javaosc-core:0.8")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package org.example
+
+import java.lang.System
+import org.apache.commons.cli.CommandLineParser
+import com.illposed.osc.transport.OSCPortOut
+
+fun main() {
+    System.setProperty("awt.useSystemAAFontSettings", "on")
+    System.setProperty("swing.aatext", "true")
+
+    var args : Array<String>
+
+    println("Yay")
+
+}
